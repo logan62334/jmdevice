95de198c81378fcd13378c7f563b4231c7c7a9e9
mafei <mafei@bilibili.com>
2022-04-17 20:52:32 +0800

    add travis ci

--- setup.py
+++ setup.py
@@ -1,13 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-#############################################
-# File Name: setup.py
-# Author: mafei
-# Mail: logan62334@gmail.com
-# Created Time:  2017-06-26 01:25:34 AM
-#############################################
-
 from setuptools import setup, find_packages
 
 import jmdevice
@@ -22,11 +12,8 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     license="MIT Licence",
-
     author="mafei",
-    author_email="logan62334@gmail.com",
     url="https://github.com/logan62334/jmdevice.git",
-
     packages=find_packages(),
     include_package_data=True,
     zip_safe=True,
